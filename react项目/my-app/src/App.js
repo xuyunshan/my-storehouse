c52ab5d8d3b908521bc777401f6d9fb6d036edb7
yunsxu <yunsxu@linkedin.com>
2022-03-28 14:57:37 +0800

    [mini-vue] vue3实战学习

--- react项目/my-app/src/App.js
+++ react项目/my-app/src/App.js
@@ -1,44 +1,78 @@
 import React, { useState, useEffect, useRef, useReducer } from "react";
 import ReactDOM from "react-dom";
 
-function Counter() {
+// function Counter() {
 
-  const initialState = {
-    count: 0,
-    step: 1,
-  };
+//   const initialState = {
+//     count: 0,
+//     step: 1,
+//   };
   
-  function reducer(state, action) {
-    const { count, step } = state;
-    if (action.type === 'tick') {
-      return { count: count + step, step };
-    } else if (action.type === 'step') {
-      return { count, step: action.step };
-    } else {
-      throw new Error();
-    }
+//   function reducer(state, action) {
+//     const { count, step } = state;
+//     if (action.type === 'tick') {
+//       return { count: count + step, step };
+//     } else if (action.type === 'step') {
+//       return { count, step: action.step };
+//     } else {
+//       throw new Error();
+//     }
+//   }
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   const { count, step } = state;
+
+//   useEffect(() => {
+//     const id = setInterval(() => {
+//       dispatch({ type: "tick" }); // Instead of setCount(c => c + step);
+//     }, 1000);
+//     return () => clearInterval(id);
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <h1>{count}</h1>
+//       <input value={step} onChange={e => {
+//         dispatch({
+//           type: 'step',
+//           step: Number(e.target.value)
+//         });
+//       }} />
+//     </>
+//   );
+// }
+// class Index extends React.Component{
+//   constructor(props){
+//       super(props)
+//       this.state={
+//           number:0
+//       }
+//   }
+//   handerClick=()=>{
+//      for(let i = 0 ;i<5;i++){
+//          setTimeout(()=>{
+//              this.setState({ number:this.state.number+1 })
+//              console.log(this.state.number)
+//          },1000)
+//      }
+//   }
+
+//   render(){
+//       return <div>
+//           <button onClick={ this.handerClick } >num++</button>
+//       </div>
+//   }
+// }
+function Index(){
+  const [ num ,setNumber ] = useState(0)
+  const handerClick=()=>{
+      for(let i=0; i<5;i++ ){
+         setTimeout(() => {
+              setNumber(num+1)
+              console.log(num)
+         }, 1000)
+      }
   }
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { count, step } = state;
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      dispatch({ type: "tick" }); // Instead of setCount(c => c + step);
-    }, 1000);
-    return () => clearInterval(id);
-  }, [dispatch]);
-
-  return (
-    <>
-      <h1>{count}</h1>
-      <input value={step} onChange={e => {
-        dispatch({
-          type: 'step',
-          step: Number(e.target.value)
-        });
-      }} />
-    </>
-  );
+  return <button onClick={ handerClick } >{ num }</button>
 }
 
-export default Counter;
+export default Index;
